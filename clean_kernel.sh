d6ebf28811fe37a1cf1717f143e4a5f9b91281ba
Lars Tesmer <kiltec@users.noreply.github.com>
2014-07-17 09:46:32 +0200

    Fixes '[: ==: unary operator expected'
    When running clean_kernel.sh, the following error message was displayed:
    ./clean_kernel.sh: line 19: [: ==: unary operator expected
    
    Even though the script still works, it's a bit irritating, so here's the fix for it.
    For more info about why it's needed see:
    http://stackoverflow.com/questions/408975/compare-integer-in-bash-unary-operator-expected/605138#605138

--- clean_kernel.sh
+++ clean_kernel.sh
@@ -16,7 +16,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 ##########################################################################
 keep=2
-if [ $1 == "1" ]; then
+if [ "$1" == "1" ]; then
     keep=1
 fi
 OLD=$(ls -tr /boot/vmlinuz-* | head -n -$keep | cut -d- -f2- | awk '{print "linux-image-" $0}')
